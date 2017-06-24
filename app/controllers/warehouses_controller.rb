6bdef4f7911c1633793fa52099f67cacb7a111c9
郑懿彬 <lance.zyb@gmail.com>
2017-06-24 23:14:18 +0800

    原型演示

---
+++ app/controllers/warehouses_controller.rb
@@ -0,0 +1,14 @@
+class WarehousesController < ApplicationController
+
+  layout "warehourses"
+
+  def index
+  end
+
+  def order
+  end
+
+  def orders
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "warehouses#index"
+  resources :warehouses, only: [:index] do
+    collection do
+      get :order
+      get :orders
+    end
+  end
 end
