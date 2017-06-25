34bd3f5ea10d6c0f0181d2ac36a719a12f5b9562
郑懿彬 <lance.zyb@gmail.com>
2017-06-25 15:19:36 +0800

    报表明细

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "warehouses#index"
+  root to: "home#index"
+  get "home/index", to: "home#index"
   resources :warehouses, only: [:index] do
     collection do
       get :order
